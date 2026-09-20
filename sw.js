7db41a35658aa2ea897bbb585fdbe78176c3f15c
eyingayannick <45818031+eyingayannick@users.noreply.github.com>
2026-09-20 07:24:16 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="enky-fit-v1";
+const FILES=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png","maskable-512.png","apple-touch-icon.png","favicon-64.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
